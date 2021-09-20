4b4e9e6a6527c7a08eced5e2b1e0c222764fd0b7
evgenya-korolkova <evgenyakorolkova@mail.ru>
2021-09-21 02:15:13 +0300

    First commit

---
+++ src/pug/pages/ui-kit/colors-type/colors-type.js
@@ -0,0 +1,3 @@
+import './colors-type.scss';
+
+console.log("[run colors-type.bundle.js]");
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDev = process.env.NODE_ENV === 'development';
+console.log('IS DEV: ', process.env.NODE_ENV);
+
+
+module.exports = {
+    entry: {
+        'colors-type': './src/pug/pages/ui-kit/colors-type/colors-type.js',
+        // entry: ['@babel/polyfill', './src/index.js'],
+    },
+
+    output: {
+        filename: '[name]/[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true, // clean output directory before emit
+    },
+
+    devServer: {
+        watchFiles: ['src/**/*.pug'], 
+        open: ['colors-type/colors-type.html'], //  --open /colors-type/colors-type.html
+    },
+
+    devtool: isDev ? 'source-map' : false,
+  
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/pug/pages/ui-kit/colors-type/colors-type.pug',
+            filename: 'colors-type/colors-type.html',
+            chunks: 'colors-type',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name]/[name].css',
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                use: ['html-loader']
+            },
+            // стили
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            // изображения
+            {
+                test: /\.(png|jpg|svg|gif)$/i,
+                type: 'asset/resource',
+                exclude: path.resolve(__dirname, 'src/fonts'),
+                generator: {
+                    filename: 'img/[name].[contenthash][ext]'
+                }
+            },
+            // шрифты
+            {
+                test: /\.(ttf|woff|svg)$/i,
+                type: 'asset/resource',
+                include: path.resolve(__dirname, 'src/fonts'),
+                generator: {
+                    filename: 'font/[name].[contenthash][ext]'
+                }
+            },
+            {
+                test: /\.pug$/i,
+                // use: ['pug-loader']
+                use: {
+                        loader: 'pug-loader',
+                        options: {
+                            // basedir: path.resolve(__dirname, 'src/pug'),
+                        }
+                    }
+            },
+            // babel
+            // {
+            //     test: /\.js$/i,
+            //     exclude: /node_modules/,
+            //     use: {
+            //         loader: 'babel-loader',
+            //         options: {
+            //             presets: ['@babel/preset-env']
+            //         }
+            //     }
+            // },
+        ]
+    },
+
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        }
+    }
+};
