3bdfc17d5af071f9f075235c36f2ca419729a722
evgenya-korolkova <evgenyakorolkova@mail.ru>
2021-10-12 22:36:43 +0300

    Added rich checkbox, expand. checkbox

---
+++ src/blocks/expand-checkbox/expand-checkbox.js
@@ -0,0 +1,24 @@
+export class Checkbox {
+    constructor(block) {
+        this.block = block
+
+        this.getDOM()
+        this.addListener()
+    }
+
+    getDOM() {
+        this.checkbox = this.block.querySelector('.expand-checkbox__checkbox')
+        this.title = this.block.querySelector('.expand-checkbox__title')
+    }
+
+    addListener() {
+        // клик мыши, Enter (title)
+        this.handleTitleClick = this.handleTitleClick.bind(this)
+        this.title.addEventListener('click', this.handleTitleClick)
+    }
+
+    handleTitleClick() {
+        this.checkbox.classList.toggle('expand-checkbox__checkbox_hidden') // показать/скрыть checkbox
+        this.title.classList.toggle('expand-checkbox__title_rotated') // повернуть стрелку
+    }
+}
--- src/pages/ui-kit/form-elements/form-elements.js
+++ src/pages/ui-kit/form-elements/form-elements.js
@@ -1,8 +1,15 @@
 import './form-elements.scss';
 
 import {Dropdown} from '@blocks/dropdown/dropdown.js';
+import {Checkbox} from '@blocks/expand-checkbox/expand-checkbox.js';
 
 document.querySelectorAll('.dropdown').forEach(function(item) {
     const dropdown = new Dropdown(item)
     // console.log(dropdown)
 })
+
+document.querySelectorAll('.expand-checkbox').forEach(function(item) {
+    const checkbox = new Checkbox(item)
+    // console.log(checkbox)
+})
+
