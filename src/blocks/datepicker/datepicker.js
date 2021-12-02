4f37cd1c2d3581a1ed46123a4668a6cdbc852eae
evgenya-korolkova <evgenyakorolkova@mail.ru>
2021-12-02 22:22:31 +0300

    Refactoring datepicker

--- src/blocks/datepicker/datepicker.js
+++ src/blocks/datepicker/datepicker.js
@@ -1,13 +1,16 @@
 import AirDatepicker from 'air-datepicker';
 
 export class Datepicker {
-    constructor(block) {
+    constructor(block, dates = []) {
         this.block = block
         this.type = block.dataset.type
+        this.dates = dates
 
         this.getDOM()
+        this.initEvent()
         this.initAirDatepicker()
         this.addListener()
+        this.setDate()
     }
 
     getDOM() {
@@ -15,50 +18,39 @@ export class Datepicker {
         this.inputTo = this.block.querySelector('[data-input="to"]')
         this.inputFilter = this.block.querySelector('[data-input="filter"]')
         this.inputReal = this.block.querySelector('.datepicker__input-real')
+        // this.btnClear =  this.adp.$datepicker.querySelector('.air-datepicker__btn-clear') см. initAirDatepicker()
+    }
+
+    initEvent() {
+        this.event = new CustomEvent('change', {
+            detail: {dates: []},
+            bubbles: true,
+            cancelable: true,
+            composed: false,
+        });
     }
 
     initAirDatepicker() {
 
         const btnApply = {
             content: 'Применить',
-            className: 'datepicker__btn-apply btn-text__button h3',
+            className: 'air-datepicker__btn-apply btn-text__button h3',
             onClick: (dp) => {
-                if (dp.dp.type === 'filter') {
-                    if (dp.selectedDates[0]) {
-                        dp.dp.inputFilter.innerText = dp.formatDate(dp.selectedDates[0], dp.opts.dateFormat)
-                        if (dp.selectedDates[1]) {
-                            dp.dp.inputFilter.innerText = dp.dp.inputFilter.innerText + ' - ' + dp.formatDate(dp.selectedDates[1], dp.opts.dateFormat)
-                        } 
-                    } else {
-                        dp.dp.inputFilter.innerText = dp.opts.defaultValue
-                    }
-
-                } else {
-                    if (dp.selectedDates[0]) {
-                        dp.dp.inputFrom.innerText = dp.formatDate(dp.selectedDates[0], dp.opts.dateFormat)
-                    } else {
-                        dp.dp.inputFrom.innerText = dp.opts.defaultValue
-                    }
-
-                    if (dp.selectedDates[1]) {
-                        dp.dp.inputTo.innerText = dp.formatDate(dp.selectedDates[1], dp.opts.dateFormat)
-                    } else {
-                        dp.dp.inputTo.innerText = dp.opts.defaultValue
-                    }
-                }
+                dp.dp.applyDate()
+                dp.dp.sendEvent()
             }
         }
         
         const btnClear = {
             content: 'Очистить',
-            className: 'datepicker__btn-clear datepicker__btn-clear_hidden btn-text__button h3',
+            className: 'air-datepicker__btn-clear btn-text__button h3',
             onClick: (dp) => {
                 dp.clear()
             }
         }
 
         const opts = {
-            range:true, 
+            range: true, 
             dateFormat: 'dd.MM.yyyy',
             showEvent: '',
             offset: 5,
@@ -68,22 +60,13 @@ export class Datepicker {
                 days: 'MMMM yyyy'
             },
 
-            // selectedDates: ['11.01.2021', '11.10.2021'], // мм.дд.гггг
-
             onSelect({date, formattedDate, datepicker}) {
-                if (datepicker.selectedDates.length > 0) {
-                    datepicker.btnClear.classList.remove('datepicker__btn-clear_hidden')
-                } else {
-                    datepicker.btnClear.classList.add('datepicker__btn-clear_hidden')
-                }
+                datepicker.dp.visibleBtnClear()
             },
 
             onChangeView(view) {
-                if (view === 'days') {
-                    this.dp.$buttons.classList.remove('air-datepicker--buttons_hidden')
-                } else {
-                    this.dp.$buttons.classList.add('air-datepicker--buttons_hidden')
-                }
+                this.adp.dp.visibleButtons(view)
+                this.adp.dp.setAirDatepickerWidth()
             },
         }
 
@@ -93,15 +76,13 @@ export class Datepicker {
         }
         
         this.adp = new AirDatepicker(this.inputReal, opts)
-        this.adp.btnClear =  this.adp.$datepicker.querySelector('.datepicker__btn-clear') // для onSelect -> скрывать/показывать Очистить
-        this.adp.dp = this          // adp.dp - ссылка на родителя (для кнопки Применить -> записать даты в соотв. поля)
-        this.adp.opts.dp = this.adp // opts.dp - ссылка на родителя (для onChangeView -> убрать кнопки Очистить и Применить)
+
+        this.adp.dp = this          // ссылка на родителя (Datepicker)
+        this.adp.opts.adp = this.adp // ссылка на родителя (AirDatepicker)
+        
         this.adp.locale.monthsShort = this.adp.locale.monthsShort.map((item) => item.toLowerCase()) // название месяца строчными буквами
 
-        // показать Очистить, если выбраны даты
-        if (this.adp.selectedDates.length > 0) {
-            this.adp.btnClear.classList.remove('datepicker__btn-clear_hidden')
-        }
+        this.btnClear =  this.adp.$datepicker.querySelector('.air-datepicker__btn-clear')
     }
 
     addListener() {
@@ -119,7 +100,74 @@ export class Datepicker {
     }
 
     handleInputClick() {
-        this.adp.show()
-        this.inputReal.focus()
+        this.adp.show()                 // показать календарь
+        this.setAirDatepickerWidth()    // установить ширину календаря
+        this.inputReal.focus()          // переключить фокус
     }
+
+    // установить даты (переданные в конструктор)
+    setDate() {
+        this.adp.selectDate(this.dates) // в AirDatepicker
+        this.applyDate()                // в Datepicker
+        this.visibleBtnClear()          // видимость кнопки 'Очистить'
+    }
+
+    // установить ширину календаря
+    setAirDatepickerWidth() {
+        this.adp.$datepicker.style.width = getComputedStyle(this.adp.$el).width
+        // this.adp.$el = this.inputReal
+    }
+
+    // отобразить в Datepicker даты, выбранные в AirDatepicker
+    applyDate() {
+        const adp = this.adp
+        if (this.type === 'filter') {
+            if (adp.selectedDates[0]) {
+                this.inputFilter.innerText = adp.formatDate(adp.selectedDates[0], adp.opts.dateFormat)
+                if (adp.selectedDates[1]) {
+                    this.inputFilter.innerText = this.inputFilter.innerText + ' - ' + adp.formatDate(adp.selectedDates[1], adp.opts.dateFormat)
+                } 
+            } else {
+                this.inputFilter.innerText = adp.opts.defaultValue
+            }
+
+        } else {
+            if (adp.selectedDates[0]) {
+                this.inputFrom.innerText = adp.formatDate(adp.selectedDates[0], adp.opts.dateFormat)
+            } else {
+                this.inputFrom.innerText = adp.opts.defaultValue
+            }
+
+            if (adp.selectedDates[1]) {
+                this.inputTo.innerText = adp.formatDate(adp.selectedDates[1], adp.opts.dateFormat)
+            } else {
+                this.inputTo.innerText = adp.opts.defaultValue
+            }
+        }
+    }
+
+    // отправить событие об изменении дат
+    sendEvent() {
+        this.event.detail.dates = this.adp.selectedDates
+        this.block.dispatchEvent(this.event);
+    }
+
+    // показать/скрыть кнопку Очистить
+    visibleBtnClear() {
+        if (this.adp.selectedDates.length > 0) {
+            this.btnClear.classList.remove('air-datepicker__btn-clear_hidden')
+        } else {
+            this.btnClear.classList.add('air-datepicker__btn-clear_hidden')
+        }
+    }
+
+    // показать/скрыть кнопки (Очистить, Применить)
+    visibleButtons(view) {
+        if (view === 'days') {
+            this.adp.$buttons.classList.remove('air-datepicker--buttons_hidden')
+        } else {
+            this.adp.$buttons.classList.add('air-datepicker--buttons_hidden')
+        }
+    }
+
 }
